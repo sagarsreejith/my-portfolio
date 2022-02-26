03c17cd74709fe8b8316d9c6f0d3d2144a8d4477
Sagar Sreejith <sreejith.sagar@gmail.com>
2022-02-26 14:56:09 +0400

    eslint (#3)
    * eslint
    
    * Fixes
    
    * eslint: use only single quotes
    
    * lined with single quotes
    
    * Update node.js.yml

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import Container from "@mui/material/Container";
+import Container from '@mui/material/Container';
 import  Navigation  from '../src/components/common/navigation';
 
 const Homepage = () => {
--- src/components/common/navigation.tsx
+++ src/components/common/navigation.tsx
@@ -1,21 +1,28 @@
-import { Box } from "@mui/material";
+import { Box } from '@mui/material'
 
 const Navigation = () => {
   const navArray = [
-    { label: "myBio()" },
-    { label: "showMyExperiance()" },
-    { label: "contactMe()" },
-    { label: "myBio()" },
-    { label: "showMyExperiance()" },
-    { label: "contactMe()" },
-  ];
+    { label: 'myBio()' },
+    { label: 'showMyExperiance()' },
+    { label: 'contactMe()' },
+    { label: 'myBio()' },
+    { label: 'showMyExperiance()' },
+    { label: 'contactMe()' },
+  ]
   return (
-    <Box sx={{ display: 'flex', justifyContent:'space-between', marginTop: 3, letterSpacing: 2}}>
-      {navArray.map((nav) => {
-        return <Box sx={{cursor: 'pointer'}}>{nav.label}</Box>;
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'space-between',
+        marginTop: 3,
+        letterSpacing: 2,
+      }}
+    >
+      {navArray.map((nav,  index ) => {
+        return <Box sx={{ cursor: 'pointer' }} key={index}>{nav.label}</Box>
       })}
     </Box>
-  );
-};
+  )
+}
 
-export default Navigation;
+export default Navigation
